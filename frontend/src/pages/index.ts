37eb404e26cece6107689ac59dedbcd1c27d2afb
xsdiablozb <cactusjackk48@gmail.com>
2026-03-16 13:12:12 +0200

    added routing on frontend site

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,7 @@
+import { HomePage } from "./HomePage";
+import { SignUpPage } from "./SignUpPage";
+import { LoginPage } from "./LoginPage";
+import { SettingsPage } from "./SettingsPage";
+import { ProfilePage } from "./ProfilePage";
+
+export { HomePage, SignUpPage, LoginPage, SettingsPage, ProfilePage };
