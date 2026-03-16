0bec71e9da9644fe06928d677eab6c2f8b13e6a9
xsdiablozb <cactusjackk48@gmail.com>
2026-03-16 12:25:26 +0200

    finished with backend part

---
+++ backend/src/controllers/message.controller.ts
@@ -0,0 +1,70 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+import type { Request, Response } from "express";
+import type { QueryFilter } from "mongoose";
+
+const getUsersForSidebar = async (req: Request, res: Response) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getUsersForSidebar: ", (error as Error).message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const getMessages = async (req: Request, res: Response) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    } as QueryFilter<typeof Message>);
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller", (error as Error).message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+const sendMessage = async (req: Request, res: Response) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    if (!text && !image) {
+      return res
+        .status(400)
+        .json({ error: "Message must contain text or an image" });
+    }
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controller", (error as Error).message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export { getUsersForSidebar, getMessages, sendMessage };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port:${PORT}`);
