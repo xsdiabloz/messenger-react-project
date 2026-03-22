9b67beed03dc3239a97489647903e18bb7f73d59
xsdiablozb <cactusjackk48@gmail.com>
2026-03-22 14:49:39 +0200

    finished

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -45,7 +45,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "frontend", "dist");
+  const rootDir = process.cwd();
+
+  const frontendPath = path.resolve(rootDir, "frontend", "dist");
 
   app.use(express.static(frontendPath));
 
